0092070a11088cd17672c2324a430e9640e0ad10
Kevin Menpara <202101079@daiict.ac.in>
2024-08-16 18:40:53 +0530

    Use django shortcut - render() to render templates

--- signUpLogin/views.py
+++ signUpLogin/views.py
@@ -7,24 +7,27 @@ from django.template import loader
 
 def signUp(request):
     try:
-        signup_template = loader.get_template("signUpLogin/signUp.html")
+        # signup_template = loader.get_template("signUpLogin/signUp.html")
         context = {"login_url":reverse('login')}
-        return HttpResponse(signup_template.render(context,request))
+        # return HttpResponse(signup_template.render(context,request))
+        return render(request,"signUpLogin/signUp.html",context)
     except:
         return HttpResponseNotFound("Error in signUp.html")
 
 def login(request):
     try:
-        login_template = loader.get_template("signUpLogin/login.html")
+        # login_template = loader.get_template("signUpLogin/login.html")
         context = {"signUp_url":reverse('signUp')}
-        return HttpResponse(login_template.render(context,request))
+        return render(request,"signUpLogin/login.html",context)
+        # return HttpResponse(login_template.render(context,request))
     except:
         return HttpResponseNotFound("Error in login.html")
 
 def thankyou(request):
     try:
-        thankyou_template = loader.get_template("signUpLogin/thankYou.html")
+        # thankyou_template = loader.get_template("signUpLogin/thankYou.html")
         context = {"home_url":reverse('login')}
-        return HttpResponse(thankyou_template.render(context,request))
+        return render(request,"signUpLogin/thankYou.html",context)
+        # return HttpResponse(thankyou_template.render(context,request))
     except:
         return HttpResponseNotFound("Error in thankYou.html")
