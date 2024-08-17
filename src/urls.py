64fb42652e6de1046d78989aad6c5b9e8d013a9a
Kevin Menpara <202101079@daiict.ac.in>
2024-08-17 10:01:47 +0530

    Final Leave

--- src/urls.py
+++ src/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/', include('allauth.urls')), 
+    path('accounts/', include('allauth.urls')), 
     path('', include('signUpLogin.urls')),
     # path('api/v1/<uuid:payment_id>',include('paymentProcess.urls')),
 ]
