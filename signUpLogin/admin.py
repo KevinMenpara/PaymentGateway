0d3c6c2f22af7e6bc631ccdcbae2aba559ddebb1
Kevin Menpara <202101079@daiict.ac.in>
2024-08-27 18:46:55 +0530

    Update: Delete user action

--- signUpLogin/admin.py
+++ signUpLogin/admin.py
@@ -10,28 +10,21 @@ from decouple import config
 
 class UserPDFInline(admin.TabularInline):
     model = UserPDF
-    extra = 0  # No extra forms for adding new PDFs
-    readonly_fields = ('pdf_file_path',)  # Make fields read-only
+    extra = 0
+    readonly_fields = ('pdf_file_path',)
 
-    def has_add_permission(self, request, obj=None):
-        return False  # Disable adding new PDF files
-
-    def has_change_permission(self, request, obj=None):
-        return False  # Disable editing existing PDF files
-
-    def has_delete_permission(self, request, obj=None):
-        return False  # Disable deleting PDF files
+    def delete_model(self, request, obj):
+        super().delete_model(request, obj)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'dob', 'expiry', 'ammount', 'created_at', 'updated_at', 'active')
     search_fields = ('name', 'email', 'dob', 'ammount')
     list_editable = ('ammount',)
-    actions = ['reset_passwords', 'mark_as_inactive', 'mark_as_active', 'export_as_csv']
-    inlines = [UserPDFInline]  # Add UserPDFInline to UserAdmin
+    actions = ['reset_passwords', 'mark_as_inactive', 'mark_as_active', 'export_as_csv', 'delete_user_pdfs']
+    inlines = [UserPDFInline]
 
     def reset_passwords(self, request, queryset):
-        """Action to reset passwords for selected users."""
         for user in queryset:
             user.set_password('new_password')  # Use a secure method for password generation
             user.save()
@@ -46,22 +39,29 @@ class UserAdmin(admin.ModelAdmin):
 
     reset_passwords.short_description = "Reset passwords for selected users"
 
+    def delete_user_pdfs(self, request, queryset):
+        count = 0
+        for user in queryset:
+            if hasattr(user, 'userpdf'):
+                user.userpdf.delete()
+                count += 1
+        self.message_user(request, _("Deleted %d UserPDFs associated with selected users." % count))
+
+    delete_user_pdfs.short_description = "Delete UserPDFs for selected users"
+
     def mark_as_inactive(self, request, queryset):
-        """Action to mark selected users as inactive."""
         queryset.update(active=False)
         self.message_user(request, _("Selected users have been marked as inactive."))
 
     mark_as_inactive.short_description = "Mark selected users as inactive"
 
     def mark_as_active(self, request, queryset):
-        """Action to mark selected users as active."""
         queryset.update(active=True)
         self.message_user(request, _("Selected users have been marked as active."))
 
     mark_as_active.short_description = "Mark selected users as active"
 
     def export_as_csv(self, request, queryset):
-        """Action to export selected users as a CSV file."""
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="users.csv"'
 
@@ -73,15 +73,19 @@ class UserAdmin(admin.ModelAdmin):
 
         return response
 
+    def delete_model(self, request, obj):
+        """Override delete_model to ensure associated UserPDFs are deleted."""
+        if hasattr(obj, 'userpdf'):
+            obj.userpdf.delete()
+        super().delete_model(request, obj)
+
     def get_queryset(self, request):
-        """Override the default queryset to show all users by default."""
         qs = super().get_queryset(request)
         if request.session.get('show_active_users', False):
-            return qs.filter(active=True)  # Show only active users
-        return qs  # Show all users
+            return qs.filter(active=True)
+        return qs
 
     def get_urls(self):
-        """Add custom URLs for showing active and all users."""
         urls = super().get_urls()
         custom_urls = [
             path('show-active-users/', self.admin_site.admin_view(self.show_active_users), name='show_active_users'),
@@ -90,19 +94,16 @@ class UserAdmin(admin.ModelAdmin):
         return custom_urls + urls
 
     def show_active_users(self, request):
-        """Action to show only active users."""
         request.session['show_active_users'] = True
         self.message_user(request, _("Showing only active users."))
-        return redirect('..')  # Redirect to the changelist view
+        return redirect('..')
 
     def show_all_users(self, request):
-        """Action to show all users."""
         request.session['show_active_users'] = False
         self.message_user(request, _("Showing all users."))
-        return redirect('..')  # Redirect to the changelist view
+        return redirect('..')
 
     def changelist_view(self, request, extra_context=None):
-        """Override changelist view to add the 'Show Active Users' and 'Show All Users' buttons."""
         extra_context = extra_context or {}
         extra_context['show_active_users'] = request.session.get('show_active_users', False)
         return super().changelist_view(request, extra_context=extra_context)
